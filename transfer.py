52438e98278e34d42dcc21b6091e11c895c54264
tushunjie <13585775243@163.com>
2017-11-25 17:31:43 +0100

    commit "prng finished"

--- transfer.py
+++ transfer.py
@@ -4,18 +4,41 @@
 #unichr(unicode)->char  
 #ord(char) ->ascii
 
+#print(ord('c'))   char->ascii 
+#print(bin(ord('c'))) char->ascii->0b 
 
+#'sep'.join(seq) 
+#str.replace(old, new[, max])
 
-def encode(s):
+lfsr_mode = [1,3]
+lfsr_init_str = [0,1,0,1,0,1,1]
+
+def encode(s): 
     return ' '.join([bin(ord(c)).replace('0b', '') for c in s])
    # return ' '.join([bin(ord(c)) for c in s])
 
 def decode(s):
     return ''.join([chr(i) for i in [int(b, 2) for b in s.split(' ')]])
-str="tushunjie"
-print(encode(str))
-#print(ord('c'))   char->ascii 
-#print(bin(ord('c'))) char->ascii->0b 
 
-#'sep'.join(seq) 
-#str.replace(old, new[, max])s 
+def lfsr(lfsr_init_str):
+    key = lfsr_init_str
+    while len(key) < 65 :
+        key.append( key[(len(key)-1)]^key[(len(key)-3)])
+        #print(key)
+    print("key generated de 64 bits:")
+    print(key)
+    return key 
+
+
+def lfsr_64bits(lfsr_init_str, lfsr_mode):
+    key = lfsr_init_str
+    new_bit = lfsr_mode[0]
+    while len(key) < 65 :
+        for i in range(1,len(lfsr_mode)): 
+            new_bit = new_bit^key[(len(key)-lfsr_mode[i])]
+        key.append(new_bit)
+        #print(key)
+    #print("key generated de 64 bits:")
+    #print(key)
+    return key
+
