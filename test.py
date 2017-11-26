495cc776dd1abb6e56b41e220127bdfc9298489f
Cheng JIANG <cheng.jiang@outlook.fr>
2017-11-26 02:05:02 +0100

    ...

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
-t = []
-print(t)
+def change(list):
+    list[0] = 2
+
+def per(list, pi):
+    t = list[0:len(list)]
+    for i in range(len(list)):
+        list[i] = t[pi[i]]
+    
+
+pi = (0,2,3,1)
+l = [2,6,7,8]
+per(l,pi)
+
+print(l)
